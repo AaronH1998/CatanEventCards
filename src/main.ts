525e625841e420dffd400a57949707be69c06a81
Aaron Hodgson <aaronhodgson1998@gmail.com>
2023-11-13 17:36:06 +0000

    Added Routing, tailwind and fontawesome

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
 import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
 
-createApp(App).mount('#app')
+import App from './App.vue'
+import router from './router';
+
+library.add(faHouse);
+
+const app = createApp(App);
+
+app.use(router);
+
+app.component('font-awesome-icon', FontAwesomeIcon);
+
+app.mount('#app');
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import HelloWorld from '../components/HelloWorld.vue';
+
+    const msg="Hello World";
+
+</script>
+
+<template>
+    <div class="flex border-b bg-red-700 text-white justify-between h-[50px] items-center p-2">
+        <span class="font-semibold text-2xl">Catan Event Cards</span>
+        <button @click="$router.go(0)"><font-awesome-icon icon="fa-solid fa-house" /></button>
+    </div>
+
+    <HelloWorld :msg="msg"/>
+</template>
+
+<style>
+</style>
